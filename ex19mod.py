fbb6a9c5b7f9100fadfcca2ca4460e58e20c0db6
Will-ChewinToBacca <will_croxford@hotmail.com>
2017-07-03 17:04:53 +0100

    Create ex19mod.py
    Recent Python 2 exercise from learning python hard way online tutorial.

---
+++ ex19mod.py
@@ -0,0 +1,17 @@
+def Calcing (Front, Rear):
+    print "Multiplying %d by %d, you get %d.\
+    " % (Front, Rear, Front * Rear)
+    print "The power of your leg to your paunch is exactly:\
+     %d." % (Front ** Rear)
+
+Calcing (10, 12)
+
+Calcing (int(raw_input("How many inches is your leg?")), int(raw_input("And your front paunch?")))
+
+x = 12 % 3 - 7
+y = 12**3
+
+Calcing (x/y , y/x)
+
+# this doesn't work... out_calcing = open("vex.txt", 'w').write(Calcing(3, 1))
+# error expects string or other character buffer object
